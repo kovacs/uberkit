5a949c23257148116bfd65c643dabb6c2241d1a0
Michael Kovacs <kovacs@gmail.com>
2009-02-06 17:34:02 -0800

    make horizontal form fields use tables for ease of layout. I know bad, bad, bad but it's just for dialog forms which are small and contained

--- lib/uberkit/forms/horizontal_builder.rb
+++ lib/uberkit/forms/horizontal_builder.rb
@@ -1,25 +1,28 @@
 class Uberkit::Forms::HorizontalBuilder < Uberkit::Forms::Builder
   def generic_field(label_text,field,content,options = {})
     required = options.delete(:required)
-    content_tag(:div, :class => "field_row#{' required' if required}#{' labelless' if label_text == ""}") do
+    content_tag(:tr, :class => "field_row#{' required' if required}#{' labelless' if label_text == ""}") do
       ret = label(field, (label_text || field.to_s.titleize).to_s + ":") unless label_text == ""
       if options[:tooltip]
         ret << build_tt_image(build_tt(field.to_s))
         ret << build_tt_container(field.to_s, options[:tooltip])
       end
-#      ret << '<br/>'
+      ret << '</td><td>'
       ret << content
       ret << content_tag(:span, options.delete(:help), :class => "help") if options[:help]
       ret << content_tag(:span, options.delete(:description), :class => "description") if options[:description]
-      ret << "<br/>"
+      ret << "</td></tr>"
       ret
     end
   end
 
-    def custom(options = {}, &block)
-    concat("<div class='field_row#{' labelless' unless options[:label]}'>#{"<label#{" for='#{options[:for]}'" if options[:for]}>#{options[:label] + ":" if options[:label]}</label>" if options[:label]}",block.binding)
+  def custom(options = {}, &block)
+#    concat("<div class='field_row#{' labelless' unless options[:label]}'>#{"<span class='label_holder'><label#{" for='#{options[:for]}'" if options[:for]}>#{options[:label] + ":" if options[:label]}</label></span>" if options[:label]}",block.binding)
+#    yield
+#    concat("</div>",block.binding)
+    concat("<tr class='field_row#{' labelless' unless options[:label]}'><td>#{"<label#{" for='#{options[:for]}'" if options[:for]}>#{options[:label] + ":" if options[:label]}</label></td><td>" if options[:label]}",block.binding)
     yield
-    concat("</div>",block.binding)
+    concat("</td></tr>",block.binding)
   end
 
 end
